90e7ae6ae244d928e983a51dd91e36c0280ff733
Ava Callery <avacallery@gmail.com>
2019-08-29 00:47:31 -0400

    functions

--- todos.js
+++ todos.js
@@ -1,5 +1,33 @@
 var toDoList = ["go to CareerDevs", "go to The Columbus", "watch OITNB"]; 
 
+//function to display to-dos
 function displayToDoList() {
     console.log("What I have to do today:", toDoList);
 }
+displayToDoList(); 
+
+//function to add to-dos
+function addToDo(todo) { 
+    toDoList.push(todo);
+    displayToDoList(); 
+}
+
+addToDo("makeout"); 
+
+//function to change to-dos 
+function changeToDo(position, newValue) {
+    toDoList[0] = "snuggle wit boo"; 
+    displayToDoList();
+}
+
+changeToDo();
+
+//function to change to delete to-dos
+function deleteToDo(position) {
+    toDoList.splice(3, 1);
+    displayToDoList()
+}
+
+deleteToDo(); 
+
+//
