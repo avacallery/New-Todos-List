04db6f05b971854a976dcd40a83441d165d3fb5e
Ava Callery <avacallery@gmail.com>
2019-08-30 18:56:45 -0400

    if else statements

--- loopstodos.js
+++ loopstodos.js
@@ -11,23 +11,42 @@
 
 var toDosObject = {
     todos: [],
-    displaytoTodos: function() {
+    displayTodos: function() {
+    if (this.todos.length === 0) {
+        console.log("Your todo list is empty!")
+    } else {
         console.log("My todos:");
-        for (var i = 0; i < this.todos.length; i++) {
-            console.log(this.todos[i].todoText); 
-
+        for (var i = 0; i < this.todos.length; i++) {           
+            if (this.todos[i].completed === true) {
+        console.log('(x)',this.todos[i].todoText); 
+            } else {
+        console.log('( )', this.todos[i].todoText); 
+    }
         }
-    },
+    }
+},
+
 
     addToDo: function(todoText) {
         this.todos.push({
             todoText: todoText,
             completed: false
         }); 
-        this.displaytoTodos(); 
+        this.displayTodos(); 
+    },
+
+    changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText; 
+    this.displayTodos();
+
+    },
+
+    deleteTodo: function(position) { 
+        this.todos.splice(position, 1); 
+        this.displayTodos();
     }
 
 }
-
-toDosObject.addToDo("first", "second"); 
-
+toDosObject.displayTodos(); 
+toDosObject.addToDo("first");  
+toDosObject.deleteTodo(0); 
